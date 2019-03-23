6d29c8d67d581f2dda715181167cbf2459046bc5
brenden williams <brendenw7@gmail.com>
2019-03-23 23:51:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,13 +3,15 @@ def reverse(word)
   rev = word.split('')
   
   word.size.times {rev_word << rev.pop}
-  rev_word.join()
+  rev_word.join
 end
 
 def reverse_each_word(str)
-  revArr = []
+  rev_Arr = []
   
   str.split(' ').each do |item|
-    
+    rev_Arr << reverse(item)
   end
+  
+  rev_Arr.join(" ")
 end
